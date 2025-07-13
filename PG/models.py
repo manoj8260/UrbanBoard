bc49e67e2bc2052d82bf94df06d9c55e0c81fac3
manoj8260 <mn047653@gmail.com>
2025-07-13 11:39:51 +0530

    urbanboard

--- PG/models.py
+++ PG/models.py
@@ -6,12 +6,12 @@ class PGlisting(models.Model):
     title=models.CharField(max_length=200)
     location=models.CharField(max_length=100)
     rent=models.PositiveIntegerField()
-    sharing_type=models.CharField(choices=[
+    sharing_type=models.CharField(max_length=250,choices=[
         ('single','Single'),
         ('double','Double'),
         ('triple','Triple'),
-    ],default=1)
-    room_type=models.CharField(choices=[('ac','AC'),('non_ac','non_AC')],default='non_ac')
+    ])
+    room_type=models.CharField(max_length=250,choices=[('ac','AC'),('non_ac','non_AC')])
     facilities=models.TextField(help_text='comma-separated list of facilities')
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
