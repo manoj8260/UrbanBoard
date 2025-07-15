c027bad407cdcc73ed6cfdd8b8f873d0a019723f
manoj8260 <mn047653@gmail.com>
2025-07-15 20:21:34 +0530

    forget password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import Signup,Signin,home,Logout
+from accounts.views import Signup,Signin,home,Logout ,password_reset_request,password_reset_done,password_reset_complete,password_reset_confirm
 
 urlpatterns = [
     path('',Signup,name='signup'),
     path('login/',Signin,name='signin'),
     path('home/',home,name='home'),
     path('logout/',Logout,name='signout'),
+    
+    #forget password  urls
+    path("password-reset/", password_reset_request, name="password_reset"),
+    path("password-reset/done/", password_reset_done, name="password_reset_done"),
+    path("reset/<uidb64>/<token>/", password_reset_confirm, name="password_reset_confirm"),
+    path("reset/done/", password_reset_complete, name="password_reset_complete"),
+
 ]
