6306c9544be19da45826a6fe2df3a1660f970af8
manoj8260 <mn047653@gmail.com>
2025-07-31 13:28:16 +0530

    google auth complete with email

--- accounts/backends.py
+++ accounts/backends.py
@@ -7,10 +7,12 @@ User = get_user_model()
 class  EmailOrPhoneBackend(ModelBackend):
     def authenticate(self, request, username = None, password = None, **kwargs):
         try:
-            user = User.objects.get(Q(email__iexact = username) | Q(phone__iexact = username))
+            user = User.objects.filter(Q(email__iexact = username) | Q(phone__iexact = username)).first()
+            print(user)
         except User.DoesNotExist:
             User().set_password(password) # mitigate timing attacks
             return None
         
-        if user.check_password(password) and   self.user_can_authenticate(user)  :     
+        if password and user.check_password(password) and   self.user_can_authenticate(user)  :     
              return user
+        return None 
